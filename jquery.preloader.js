41a508ea988afa2192775e5aa2951ffa1d3fcbe6
BinaryKitten <Kathryn@binaryKitten.com>
2012-03-16 22:15:22 +0000

    v0.96    Thanks to the following people for their comments and fixes:       	Evgeni Nabokov       	Matt Fordham         fanton 	Bruno Couto
       Adjusted the regex so that:
           - it will not match Quotes within the url
           - it does not match data uris which need no preloading
       Added in a few addition checks and clean ups

--- jquery.preloader.js
+++ jquery.preloader.js
@@ -1,4 +1,4 @@
-/* jQuery.preloader - v0.95 - K Reeve aka BinaryKitten
+/* jQuery.preloader - v0.96 - K Reeve aka BinaryKitten
 *
 *	Preloads a list of images, or all css linked images or both
 *	$.preLoadImages(array imageList,fn callback)
@@ -14,6 +14,18 @@
 *
 *
 * ------------ Version History -----------------------------------
+* v0.96
+*   Thanks to the following people for their comments and fixes:
+*      	Evgeni Nabokov
+*      	Matt Fordham
+*       fanton
+*		Bruno Couto
+*
+*   Adjusted the regex so that:
+*       - it will not match Quotes within the url
+*       - it does not match data uris which need no preloading 
+*   Added in a few addition checks and clean ups
+*
 * v0.95
 * 	# Note - keeping below v1 as really not sure that I consider it public usable.
 * 	# But it saying that it does the job it was intended to do.
@@ -57,83 +69,88 @@
 						};
 						pic[i].src = imageList[i];
 					}
-			}
-			else {
+			} else {
 				pic[0] = new Image();
-				pic[0].onload = function() {
-					if ($.isFunction(callback)) {
-						callback();
-					}
+				if ($.isFunction(callback)) {
+					pic[0].onload = callback;
 				}
 				pic[0].src = imageList;
 			}
+		} else if ($.isFunction(callback)) {
+			//nothing passed but we have a callback.. so run this now
+			//thanks to Evgeni Nobokov
+			callback();
 		}
 		pic = undefined;
 	};
 	
 	$.preLoadCSSImages = function(callback) {
-		var pic = [], i, imageList = [], loaded = 0, total, regex = new RegExp("url\((.*)\) ",'i'),spl;
-		var cssSheets = document.styleSheets, path,myRules,Rule,match,txt,img,sheetIdx,ruleIdx;
+		var pic = [], i, imageList = [], loaded = 0, total, regex = /url\((?:"|')?(?!data:)([^)"']+)(?:"|')?\)/i,spl;
+		var cssSheets = document.styleSheets, path,myRules='',Rule,match,txt,img,sheetIdx,ruleIdx;
 		for (sheetIdx=0;sheetIdx < cssSheets.length;sheetIdx++){
 			var sheet = cssSheets[sheetIdx];
 			if (typeof sheet.href == 'string' && sheet.href.length > 0) {
 				spl = sheet.href.split('/');spl.pop();path = spl.join('/')+'/';
-			}
-			else {
+			} else {
 				path = './';
 			}
-			myRules = sheet.cssRules ? sheet.cssRules : sheet.rules;
-			for (ruleIdx=0;ruleIdx<myRules.length;ruleIdx++) {
-				Rule = myRules[ruleIdx];
-				txt = Rule.cssText ? Rule.cssText : Rule.style.cssText;
-				match = regex.exec(txt);
-				if (match != null) {
-					img = match[1].substring(1,match[1].indexOf(')',1));
-					if (img.substring(0,4) == 'http') {
-						imageList[imageList.length] = img;
+			if (sheet.cssRules) {
+				myRules = sheet.cssRules;
+			} else if (sheet.rules) {
+				myRules = sheet.rules;
+			}
+			if (myRules.length > 0) {
+				for (ruleIdx=0;ruleIdx<myRules.length;ruleIdx++) {
+					Rule = myRules[ruleIdx];
+					txt = Rule.cssText ? Rule.cssText : Rule.style.cssText;
+					match = regex.exec(txt);
+					if (match != null) {
+						img = match[1].substring(1,match[1].indexOf(')',1));
+						if (img.substring(0,4) == 'http') {
+							imageList[imageList.length] = img;
+						} else if ( match[1].substring(1,2) == '/') {
+							var p2 = path.split('/');p2.pop();p2.pop();p2x = p2.join("/");
+							imageList[imageList.length] = p2x+img;
+						} else {
+							imageList[imageList.length] = path+img;
+						}
 					}
-					else if ( match[1].substring(1,2) == '/') {
-						var p2 = path.split('/');p2.pop();p2.pop();p2x = p2.join("/");
-						imageList[imageList.length] = p2x+img;
-					}
-					else {
-						imageList[imageList.length] = path+img;
-					}
-				}
-			};
+				};
+			} 
 		};
 		
 		total = imageList.length; // used later
-		for (i=0; i < total; i++) {
-			pic[i] = new Image();
-			pic[i].onload = function() {
-				loaded++; // should never hit a race condition due to JS's non-threaded nature
-				if (loaded == total) {
-					if ($.isFunction(callback)) {
-						callback();
+		if (total > 0) {
+			for (i=0; i < total; i++) {
+				pic[i] = new Image();
+				pic[i].onload = function() {
+					loaded++; // should never hit a race condition due to JS's non-threaded nature
+					if (loaded == total) {
+						if ($.isFunction(callback)) {
+							callback();
+						}
 					}
-				}
-			};
-			pic[i].src = imageList[i];
+				};
+				pic[i].src = imageList[i];
+			}
+		} else if($.isFunction(callback)) {
+			//nothing found, but we have a callback.. so run this now
+			//thanks to Evgeni Nobokov
+			callback();
 		}
-		
 	};
 	$.preLoadAllImages = function(imageList,callback) {
 		if (typeof imageList != 'undefined') {
 			if ($.isFunction(imageList)) {
 				callback = imageList;
-			}
-			else if (!$.isArray(imageList)) {
+			} else if (!$.isArray(imageList)) {
 				imageList = [imageList];
 			}
 		}
 		$.preLoadCSSImages(function(){
 			if (imageList.length > 0) {
-				$.preLoadImages(imageList,function(){
-					callback();
-				});
-			}
-			else {
+				$.preLoadImages(imageList,callback);
+			} else {
 				callback();
 			}
 		});
